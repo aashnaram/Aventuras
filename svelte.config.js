86bd6bce12bdd6875f3cbd37505650ea29d9e37d
Aashna Ram <aashnaram89@gmail.com>
2026-06-28 05:56:08 -0700

    chore: switch to @sveltejs/adapter-vercel for Vercel deployments

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,13 @@
-// Tauri doesn't have a Node.js server to do proper SSR
-// so we use adapter-static with a fallback to index.html to put the site in SPA mode
-// See: https://svelte.dev/docs/kit/single-page-apps
-// See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
-import adapter from '@sveltejs/adapter-static'
+// Vercel adapter for SvelteKit. Using the Vercel adapter makes deployments to Vercel seamless.
+// See: https://github.com/sveltejs/kit/tree/master/packages/adapter-vercel
+import vercel from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      fallback: 'index.html',
-    }),
+    adapter: vercel(),
   },
 }
 
